54763093f7441d61bb64a571974a4a33557bc3df
guihblac <76455460+guihblac@users.noreply.github.com>
2024-12-13 14:38:42 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@ var x = setInterval(function(){
     var hours = Math.floor(distance % (1000 * 60 * 60 *24) / (1000 * 60 * 60));
     var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
     var seconds = Math.floor(distance % (1000 * 60) / 1000);
+    var milliseconds = distance % 1000;
 
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("milliseconds").innerHTML = milliseconds;
 },1000);
 
 
